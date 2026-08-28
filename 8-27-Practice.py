ec2730c5ed0cf9663ff43a201cf9710fffba2a43
Chloe Gill <cekjohio@gmail.com>
2026-08-27 20:06:42 -0400

    Add practice from 8/27

---
+++ 8-27-Practice.py
@@ -0,0 +1,29 @@
+# Experiment with Python
+a = 23
+a * 13
+
+# Write a simple function with Python
+def multiplyIntegers(x, y):
+    return x * y
+
+a = 23
+b = 13
+multiplyIntegers(a, b)
+
+# See how type changing works if you change b
+b = "hello"
+multiplyIntegers(a, b)
+
+# See how types are identified
+type(a)
+type(b)
+type(456)
+type(3.14)
+type(7.0)
+type(False)
+type("Rossum")
+
+# See type casting
+int(3.2)
+int(3)
+3.2 + 3
