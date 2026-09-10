ab3d4d6f573212e5fb0d14885879bc9f63ef9b4b
Chloe Gill <cekjohio@gmail.com>
2026-09-10 18:45:31 -0400

    Added slides answer to 9/10 Practice exercise 1

--- 9-10-Practice.py
+++ 9-10-Practice.py
@@ -21,6 +21,7 @@ else:
 cart_total = 120
 is_member - True
 has_coupon = False
+
 if (cart_ total > 100 and is_member):
     print("Member discount: 20% off + free shipping!")
 if (cart_total > 50 and is_member):
@@ -32,4 +33,31 @@ if (has_coupon and not is_member):
 if (cart_total > 100 and not is_member):
     print("Non-member discount: 5% off + free shipping!")
 if (cart_total < 100 and not is_member)
+    print("No discounts available. Sign up for membership!")
+
+# Slides answer
+cart_total = 120
+is_member - True
+has_coupon = False
+
+# Precompute possible conditions
+member_and_cart_over_100 = is_member and (cart_total > 100)
+member_and_cart_over_50 = is_member and (cart_total > 50)
+member_and_cart_under_50 = is_member and (cart_total <= 50)
+
+non_member_and_has_coupon = not is_member and has_coupon
+non_member_and_cart_over_100 = not is_member and (cart_total > 100)
+non_member_and_cart_under_100 = not is_member and (cart_total <= 100) and (not has_coupon)
+
+if member_and_cart_over_100:
+    print("Member discount: 20% off + free shipping!")
+if member_and_cart_over_50:
+    print("Member discount: 10% off!")
+if member_and_cart_under_50:
+    print("Member discount: 5% off!")
+if non_member_and_has_coupon:
+    print("Coupon discount: 10% off!")
+if non_member_and_cart_over_100:
+    print("Non-member discount: 5% off + free shipping!")
+if non_member_and_cart_under_100:
     print("No discounts available. Sign up for membership!")
