ab512499c1ed91c14167dd156080bf2fadb61f62
Chloe Gill <cekjohio@gmail.com>
2026-09-24 18:48:30 -0400

    Adjusted bisection method + added while loop attempt

--- In-Class Exercises/Exercise-1-Bisection-924.py
+++ In-Class Exercises/Exercise-1-Bisection-924.py
@@ -1,7 +1,7 @@
 def f(x): 
     return x**3 - x - 2
 
-def bisection(low, high, err):
+def bisection(a, b, tol=1e-6, max_iter=100):
     if f(a) * f(b) >= 0:
         print("Bisection method fails. f(a) and f(b) must have opposite signs.")
         return None
@@ -17,7 +17,22 @@ def bisection(low, high, err):
     return c # best guss after max iter
 
 # Try it
-root = bisection( 1, 2)
+root = bisection(1, 2)
 print("Approx root: ", root)
 
-  
+# Turn into a while loop variant for funsies?
+def bisection(a, b, tol=1e-6, max_iter=100):
+    if f(a) * f(b) >= 0:
+        print("Bisection method fails. f(a) and f(b) must have opposite signs.")
+        return None
+    
+    while (max_iter > 0):
+        c = (a + b) / 2 # midpoint
+        if f(c) == 0 or (b - a) / 2 < tol:
+            return c # root found
+        elif f(a) * f(c) < 0:
+            b = c
+        else:
+            a = c
+        max_iter - 1
+    return c # best guess
